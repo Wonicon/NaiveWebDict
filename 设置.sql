25bc2938d65b97e85c1e304fe57186c24552691b
onts0624 <1055685656@qq.com>
2016-12-06 20:18:30 +0900

    Update 设置.sql

--- 设置.sql
+++ 设置.sql
@@ -1,4 +1,7 @@
-create database naive2;
+set password =password("onts0624");//这个是设置密码，我的代码那边设置的是onts0624
+grant all privileges on naive2.* to 'root'@'%';//这个是设置远程可以使用root以不用密码的形式访问naive2库中的所有表
+FLUSH PRIVILEGES;//使得上面的改变生效
+create database naive2;//开始创建表格
 use naive2;
 create table count
 (
