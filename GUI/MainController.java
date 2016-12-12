8e0e9d073cabdd2b5c4692847826b5a74fec7082
Huizhe Wang <conciselove@outlook.com>
2016-12-12 21:54:02 +0800

    Merge spider component

--- GUI/MainController.java
+++ GUI/MainController.java
@@ -1,10 +1,15 @@
 package GUI;
 
+import Dictionary.BaiduDict;
+import Dictionary.BingDict;
+import Dictionary.Dict;
+import Dictionary.NetEaseDict;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
@@ -12,6 +17,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainController {
+  @FXML public HBox BingBox;
+
+  @FXML public HBox BaiduBox;
+
+  @FXML public HBox NetEaseBox;
+
   @FXML private CheckBox bingSel, baiduSel, netEaseSel;
 
   @FXML private TextField word;
@@ -22,11 +33,19 @@ public class MainController {
 
   private final String bing = "Bing", baidu = "Baidu", netEase = "NetEase";
 
+  private Dict bingDict = new BingDict();
+
+  private Dict baiduDict = new BaiduDict();
+
+  private Dict netEaseDict = new NetEaseDict();
+
   private Map<String, Integer> dictMap = new HashMap<>();
 
   private Map<String, Label> labelMap = new HashMap<>();
 
   private Map<String, Button> btnMap = new HashMap<>();
+
+  private Map<String, Dict> spiderMap = new HashMap<>();
 
   @FXML
   public void initialize() {
@@ -42,6 +61,10 @@ public class MainController {
     btnMap.put(bing, bingBtn);
     btnMap.put(baidu, baiduBtn);
     btnMap.put(netEase, netEaseBtn);
+
+    spiderMap.put(bing, bingDict);
+    spiderMap.put(baidu, baiduDict);
+    spiderMap.put(netEase, netEaseDict);
 
     bingSel.setId(bing);
     baiduSel.setId(baidu);
@@ -59,11 +82,17 @@ public class MainController {
     ArrayList<Integer> dicts = new ArrayList<>();
     ArrayList<Label> labels = new ArrayList<>();
     ArrayList<Button> btns = new ArrayList<>();
+    ArrayList<Thread> tasks = new ArrayList<>();
+    ArrayList<Dict> spiders = new ArrayList<>();
     for (CheckBox cb : new CheckBox[] { bingSel, baiduSel, netEaseSel }) {
       if (cb.isSelected()) {
         dicts.add(dictMap.get(cb.getId()));
         labels.add(labelMap.get(cb.getId()));
         btns.add(btnMap.get(cb.getId()));
+        Dict dict = spiderMap.get(cb.getId());
+        dict.setWord(word.getText());
+        tasks.add(new Thread(dict));
+        spiders.add(dict);
       }
     }
 
@@ -86,6 +115,17 @@ public class MainController {
         }
       }
     }));
+
+    tasks.forEach(Thread::start);
+    try {
+      for (Thread task : tasks) {
+        task.join();
+      }
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    spiders.forEach(System.out::println);
   }
 
   private void like(String dict) {
