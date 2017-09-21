1cef4eabe5592d2bfdf14a00bdaef64f04aa394f
Huizhe Wang <conciselove@outlook.com>
2017-09-21 22:54:00 +0800

    Add Glossary model to store word entries

---
+++ src/main/java/Glossary/GlossaryCSV.java
@@ -0,0 +1,26 @@
+package Glossary;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GlossaryCSV {
+  private StringBuilder csv = new StringBuilder();
+
+  public void addEntry(String word, String pos, String def, String source, String sentence) {
+    csv.append(String.join(", ", word, pos, def, source, sentence)).append("\n");
+  }
+
+  public String export(String filename) {
+    try (FileWriter writer = new FileWriter(filename)) {
+      writer.write(csv.toString());
+      return "Success";
+    }
+    catch (IOException e) {
+      return e.toString();
+    }
+  }
+
+  public String view() {
+    return csv.toString();
+  }
+}
